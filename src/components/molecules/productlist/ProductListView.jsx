1890d0020ae1988c83cf66f2bd1e1bdfe8e6f8fa
Cristina Romero <criromllo@gmail.com>
2025-02-05 20:08:30 +0100

    quick fix: product url in product card

--- src/components/molecules/productlist/ProductListView.jsx
+++ src/components/molecules/productlist/ProductListView.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom'; 
 import "./ProductListView.css";
 
 const ProductListView = ({ productos }) => {  
@@ -8,13 +9,15 @@ const ProductListView = ({ productos }) => {
             <div className="cards-container">
                 {productos.map((producto) => (
                     <div className="Card" key={producto.id}>
-                        <div className="product-cover">
-                            <img src={producto.url_image} alt={producto.name} />
-                        </div>
-                        <div className="product-info">
-                            <p className="product-name">{producto.name}</p>
-                            <p className="product-price">{producto.price} €</p>
-                        </div>
+                        <Link to={`/plantas/${producto.id}`} className="product-link"> 
+                            <div className="product-cover">
+                                <img src={producto.url_image} alt={producto.name} />
+                            </div>
+                            <div className="product-info">
+                                <p className="product-name">{producto.name}</p>
+                                <p className="product-price">{producto.price} €</p>
+                            </div>
+                        </Link>
                     </div>
                 ))}
             </div>
